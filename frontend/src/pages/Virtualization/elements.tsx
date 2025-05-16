cc4cd580b403b702bb250c4d4ceb8c28835c388b
ehtishamali042 <85542150+ehtisham-ali-emumba@users.noreply.github.com>
2025-05-16 15:07:18 +0500

    installs react-window

--- frontend/src/pages/Virtualization/elements.tsx
+++ frontend/src/pages/Virtualization/elements.tsx
@@ -1,26 +1,18 @@
 import styled from "styled-components";
 import { Wrapper } from "../../styles";
+import { Typography } from "antd";
 
 export const Container = styled(Wrapper)`
   justify-content: flex-start;
 `;
 
-export const Title = styled.h1`
-  font-size: 2rem;
-  font-weight: 700;
-  color: #333;
-  margin-bottom: 1.5rem;
-  text-align: center;
-`;
-
 export const Box = styled.div({
-  marginTop: "150px",
+  marginTop: "110px",
   width: "100%",
   paddingBottom: "82px",
 });
 
-export const Heading = styled(Title)({
-  textAlign: "left",
+export const Heading = styled(Typography.Title)({
+  textAlign: "center",
   marginBottom: "40px",
-  marginLeft: "12px",
 });
