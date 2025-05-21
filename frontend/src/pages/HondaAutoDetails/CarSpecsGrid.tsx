ec11edaa3b4438150ab68bcd954915d25b56b81d
ehtishamali042 <85542150+ehtisham-ali-emumba@users.noreply.github.com>
2025-05-21 16:29:14 +0500

    uses virtualization

--- frontend/src/pages/HondaAutoDetails/CarSpecsGrid.tsx
+++ frontend/src/pages/HondaAutoDetails/CarSpecsGrid.tsx
@@ -8,6 +8,10 @@ import {
 } from "./elements";
 import { deleteIconStyle, editIconStyle, getHondaAutoSpecs } from "./utils";
 import type { HondaAuto } from "../../atoms/hondaAutosAtom";
+import {
+  FixedSizeList as List,
+  type ListChildComponentProps,
+} from "react-window";
 
 type CarSpecsGridType = {
   auto: HondaAuto;
@@ -16,9 +20,9 @@ type CarSpecsGridType = {
     key?: string,
     value?: string | number
   ) => void;
-
   handleDeleteClick: (key: string) => void;
 };
+
 export const CarSpecsGrid: React.FC<CarSpecsGridType> = ({
   auto,
   handleAddEditClick,
@@ -26,29 +30,32 @@ export const CarSpecsGrid: React.FC<CarSpecsGridType> = ({
 }) => {
   const carSpecs = getHondaAutoSpecs(auto);
 
+  const Row = ({ index, style }: ListChildComponentProps) => {
+    const spec = carSpecs[index];
+    return (
+      <div style={style} key={spec.label}>
+        <SpecItem>
+          <SpecLabel>{spec.label}</SpecLabel>
+          <SpecValue>{spec.value}</SpecValue>
+          <SpecActions>
+            <EditOutlined
+              style={editIconStyle}
+              onClick={() => handleAddEditClick(true, spec.label, spec.value!)}
+            />
+            <DeleteOutlined
+              style={deleteIconStyle}
+              onClick={() => handleDeleteClick(spec.label)}
+            />
+          </SpecActions>
+        </SpecItem>
+        {index < carSpecs.length - 1 && <Divider />}
+      </div>
+    );
+  };
+
   return (
-    <>
-      {carSpecs.map((spec, index) => (
-        <div key={spec.label}>
-          <SpecItem>
-            <SpecLabel>{spec.label}</SpecLabel>
-            <SpecValue>{spec.value}</SpecValue>
-            <SpecActions>
-              <EditOutlined
-                style={editIconStyle}
-                onClick={() =>
-                  handleAddEditClick(true, spec.label, spec.value!)
-                }
-              />
-              <DeleteOutlined
-                style={deleteIconStyle}
-                onClick={() => handleDeleteClick(spec.label)}
-              />
-            </SpecActions>
-          </SpecItem>
-          {index < carSpecs.length - 1 && <Divider />}
-        </div>
-      ))}
-    </>
+    <List height={350} itemCount={carSpecs.length} itemSize={56} width="100%">
+      {Row}
+    </List>
   );
 };
